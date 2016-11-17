64052d06dbd5544fd24ea5dadd065cee0b6f273d
黄位恒 <942428642@qq.com>
2016-11-17 16:57:11 +0800

    Merge remote-tracking branch 'refs/remotes/origin/master' into Vhen
    # Conflicts:
    #	common/js/app.min.js
    #	pc/pc_common/js/app.js
    #	pc/pc_common/js/app.min.js
    #	pc/pc_project/style/jsy.css
    #	pc/pc_scss/pages/_jsy-news.scss
    #	pc/pc_scss/pages/_jsy-profile.scss

--- pc/pc_common/js/app.js
+++ pc/pc_common/js/app.js
@@ -1,6 +1,3 @@
 //@koala-prepend '../jsy/jsy_index.js'
-<<<<<<< HEAD
 //@koala-prepend '../jsy/jsy_hold_index.js'
-=======
 //@koala-prepend '../jsy/jsy_changePage.js'
->>>>>>> refs/remotes/origin/master
